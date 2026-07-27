c77d28c9cdc03429652a068c960a606ebd6bebcb
Hitesh <patiyal.hitesh@example.com>
2026-07-27 21:17:02 +0530

    Switch case and ternary operator

---
+++ Basics/ConditionalStatement/SwitchCase.js
@@ -0,0 +1,18 @@
+let day = 'Fridayy'
+switch (day) {
+    case 'Monday': console.log("today is Monday")
+        break
+    case 'Tuesday': console.log("today is Tuesday")
+        break
+    case 'Wednesday': console.log("today is Wednesday")
+        break
+    case 'Thursday': console.log("today is Thursday")
+        break
+    case 'Friday': console.log("today is Friday")
+        break
+    case 'Saturday': console.log("today is Saturday")
+        break
+    case 'Sunday': console.log("today is Sunday")
+        break
+    default: console.log("Invalid day input")
+}
---
+++ Basics/ConditionalStatement/TernaryOperator.js
@@ -0,0 +1,4 @@
+// condition ? a : b
+let age = 18
+const result = age >= 18 ? "Adult" : "Child"
+console.log(result)
