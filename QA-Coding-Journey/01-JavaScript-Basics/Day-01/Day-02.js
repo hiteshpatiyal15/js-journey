85f5c4b779f7c50d2ad6dd792a668648ce5819a3
Hitesh <patiyal.hitesh@example.com>
2026-09-10 20:42:29 +0530

    Day 2 for of loop

---
+++ QA-Coding-Journey/01-JavaScript-Basics/Day-01/Day-02.js
@@ -0,0 +1,57 @@
+// Loops +  Array methods
+// For loops
+const testCases = [
+    "Login Test",
+    "Logout Test",
+    "Search Test",
+    "Add to Card Test",
+    "Checkout Test"
+];
+// executing test cases 
+for (let testCase of testCases) {
+    console.log(`Executing: ${testCase}`);
+}
+let count = 1;
+for (let testCase of testCases) {
+    console.log(`Test ${count}: ${testCase}`);
+    count++
+}
+
+// Find failed tests
+const results = [
+    "PASS",
+    "PASS",
+    "FAIL",
+    "PASS",
+    "PASS",
+    "FAIL",
+];
+
+//count the test cases passed, failed and totals
+let failCount = 0;
+let passCount = 0;
+let totalCases = 0;
+for (let result of results) {
+    if (result === "FAIL") {
+        failCount++;
+        totalCases++
+    } else if(result === "PASS"){
+        passCount++;
+        totalCases++
+    } 
+}
+
+console.log(`Total Tests: ${totalCases}`);
+console.log(`Passed Tests: ${passCount}`);
+console.log(`Failed Tests: ${failCount}`);
+
+// print the failed test numbers
+let testCount = 1;
+
+for (let result of results) {
+    if (result != "PASS")
+        console.log(`Failed Test: ${testCount}`);
+    testCount++;
+}
+
+// 
